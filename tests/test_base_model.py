f877a5c200d1da3c9df71d24bd5bf227e92495f8
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-02-17 23:33:01 +0000

    adding

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,14 +1,17 @@
-#!/usr/bin/python3
+import unittest
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+class TestBaseModel(unittest.TestCase):
+    def test_new_instance_added_to_storage(self):
+        storage = FileStorage()
+        initial_count = len(storage.all())  # Get the initial count of objects in storage
+        obj = BaseModel()
+        # Check that the object is added to storage after instantiation
+        self.assertEqual(len(storage.all()), initial_count + 1)
+        self.assertIn("BaseModel." + obj.id, storage.all())  # Check if object is in storage
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_file_storage.py
+++ tests/test_file_storage.py
@@ -1,9 +1,34 @@
 #!/usr/bin/python3
 import unittest
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
-"""This is a test on the file_storage"""
+
+class TestFileStorage(unittest.TestCase):
+    def setUp(self):
+        # Initialize a FileStorage instance
+        self.storage = FileStorage()
+
+    def test_new_and_all(self):
+        # Test if adding a new BaseModel instance results in it being in the storage
+        obj = BaseModel()
+        self.storage.new(obj)
+        self.assertIn("BaseModel." + obj.id, self.storage.all())
+
+    def test_save_and_reload(self):
+        # Test if saving and reloading retains the same objects
+        obj = BaseModel()
+        self.storage.new(obj)
+        self.storage.save()
+
+        new_storage = FileStorage()
+        new_storage.reload()
+        self.assertIn("BaseModel." + obj.id, new_storage.all())
+
+    def tearDown(self):
+        # Clean up any temporary files or resources
+        pass
 
 
-class TestStorage(unittest.TestCase):
-    """Test starts"""
-    def test_
+if __name__ == '__main__':
+    unittest.main()
