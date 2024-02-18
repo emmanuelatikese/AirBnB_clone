64f507676a61daa3868d2979094d72b82f218999
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-02-18 13:37:25 +0000

    adding

--- console.py
+++ console.py
@@ -8,8 +8,11 @@ from models.base_model import BaseModel
 class HBNBCommand(cmd.Cmd):
     '''This is all about cmd'''
     prompt = '(hbnb) '
+
     def do_quit(self, arg):
         '''This quits the command line'''
         return True
+
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
